d31669af3fae48b95634626bee356a6d9ceb7c89
farhanswitch <muhammad_farhan@outlook.co.id>
2022-07-06 22:29:36 +0700

    add contact page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../src/components/Layout";
-import { Provider } from "react-redux";
 import { wrapper } from "../src/app/store";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
