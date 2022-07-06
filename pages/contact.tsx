50cd8091e47ecea1d940f4aacea1ab6f7c80c76e
farhanswitch <muhammad_farhan@outlook.co.id>
2022-07-06 22:51:41 +0700

    add title to every pages

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,30 +1,38 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import ContactCard from "../src/components/ContactCard";
 
 const Contact: NextPage = () => {
   return (
-    <div className="flex items-center">
-      <ul className="grid grid-cols-1 place-items-center gap-8 py-20 mx-auto">
-        <ContactCard
-          social_media={"email"}
-          color={"bg-[#dbbc8e]"}
-          link={"mailto://farhan.muhammad11@programmer.net"}
-          text={"farhan.muhammad11@programmer.net"}
-        />
-        <ContactCard
-          social_media={"github"}
-          color={"bg-[#a15d98]"}
-          link={"https://github.com/farhanswitch"}
-          text={"Github : FarhanSwitch"}
-        />
-        <ContactCard
-          social_media={"linkedIn"}
-          color={"bg-[#045c8a]"}
-          link={"https://linkedin.com/in/farhanswitch"}
-          text={"LinkedIn : FarhanSwitch"}
-        />
-      </ul>
-    </div>
+    <>
+      <Head>
+        <title>SwitchCov | Kontak Saya</title>
+        <meta name="description" content="Indonesian covid 19 data" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="flex items-center">
+        <ul className="grid grid-cols-1 place-items-center gap-8 py-20 mx-auto">
+          <ContactCard
+            social_media={"email"}
+            color={"bg-[#dbbc8e]"}
+            link={"mailto://farhan.muhammad11@programmer.net"}
+            text={"farhan.muhammad11@programmer.net"}
+          />
+          <ContactCard
+            social_media={"github"}
+            color={"bg-[#a15d98]"}
+            link={"https://github.com/farhanswitch"}
+            text={"Github : FarhanSwitch"}
+          />
+          <ContactCard
+            social_media={"linkedIn"}
+            color={"bg-[#045c8a]"}
+            link={"https://linkedin.com/in/farhanswitch"}
+            text={"LinkedIn : FarhanSwitch"}
+          />
+        </ul>
+      </div>
+    </>
   );
 };
 
--- pages/harian.tsx
+++ pages/harian.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import { useState, useMemo, ChangeEvent } from "react";
 import { setCovidData } from "../src/app/features/covid/data-covid";
 import { wrapper } from "../src/app/store";
@@ -51,88 +52,99 @@ const Harian: NextPage<IPropsHarian> = ({ dataCovid, isError }) => {
   const handleOnChangeDate = (event: ChangeEvent<HTMLInputElement>) =>
     setDate(event.target.value);
   return (
-    <main className="px-2 mb-6">
-      <div className="w-full rounded-md my-6 border-blue-700">
-        <div className="flex px-4 py-2 items-center justify-between shadow-md shadow-slate-400/60">
-          <h2 className="font-semibold text-blue-600">Data Harian</h2>
-          <div className="flex">
-            <form action="submit">
-              <input
-                type="date"
-                min={
-                  arrayHarian
-                    ? arrayHarian[0].key_as_string.split("T")[0]
-                    : "2020-03-02"
+    <>
+      <Head>
+        <title>SwitchCov | Kasus Harian</title>
+        <meta name="description" content="Indonesian covid 19 data" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="px-2 mb-6">
+        <div className="w-full rounded-md my-6 border-blue-700">
+          <div className="flex px-4 py-2 items-center justify-between shadow-md shadow-slate-400/60">
+            <h2 className="font-semibold text-blue-600">Data Harian</h2>
+            <div className="flex">
+              <form action="submit">
+                <input
+                  type="date"
+                  min={
+                    arrayHarian
+                      ? arrayHarian[0].key_as_string.split("T")[0]
+                      : "2020-03-02"
+                  }
+                  max={
+                    arrayHarian &&
+                    arrayHarian[arrayHarian.length - 1].key_as_string.split(
+                      "T"
+                    )[0]
+                  }
+                  onChange={handleOnChangeDate}
+                />
+              </form>
+            </div>
+          </div>
+          <div className="my-8 px-4 py-2">
+            <CaseContainer>
+              <CaseCard
+                status="positif"
+                jumlah={
+                  filteredData ? filteredData["jumlah_positif"]["value"] : 0
                 }
-                max={
-                  arrayHarian &&
-                  arrayHarian[arrayHarian.length - 1].key_as_string.split(
-                    "T"
-                  )[0]
-                }
-                onChange={handleOnChangeDate}
               />
-            </form>
+              <CaseCard
+                status="positif"
+                jumlah={
+                  filteredData ? filteredData["jumlah_positif_kum"]["value"] : 0
+                }
+                isKum={true}
+              />
+              <CaseCard
+                status="dirawat"
+                jumlah={
+                  filteredData ? filteredData["jumlah_dirawat"]["value"] : 0
+                }
+              />
+              <CaseCard
+                status="dirawat"
+                jumlah={
+                  filteredData ? filteredData["jumlah_dirawat_kum"]["value"] : 0
+                }
+                isKum={true}
+              />
+
+              <CaseCard
+                status="sembuh"
+                jumlah={
+                  filteredData ? filteredData["jumlah_sembuh"]["value"] : 0
+                }
+              />
+              <CaseCard
+                status="sembuh"
+                jumlah={
+                  filteredData ? filteredData["jumlah_sembuh_kum"]["value"] : 0
+                }
+                isKum={true}
+              />
+
+              <CaseCard
+                status="meninggal"
+                jumlah={
+                  filteredData ? filteredData["jumlah_meninggal"]["value"] : 0
+                }
+              />
+              <CaseCard
+                status="meninggal"
+                jumlah={
+                  filteredData
+                    ? filteredData["jumlah_meninggal_kum"]["value"]
+                    : 0
+                }
+                isKum={true}
+              />
+            </CaseContainer>
           </div>
         </div>
-        <div className="my-8 px-4 py-2">
-          <CaseContainer>
-            <CaseCard
-              status="positif"
-              jumlah={
-                filteredData ? filteredData["jumlah_positif"]["value"] : 0
-              }
-            />
-            <CaseCard
-              status="positif"
-              jumlah={
-                filteredData ? filteredData["jumlah_positif_kum"]["value"] : 0
-              }
-              isKum={true}
-            />
-            <CaseCard
-              status="dirawat"
-              jumlah={
-                filteredData ? filteredData["jumlah_dirawat"]["value"] : 0
-              }
-            />
-            <CaseCard
-              status="dirawat"
-              jumlah={
-                filteredData ? filteredData["jumlah_dirawat_kum"]["value"] : 0
-              }
-              isKum={true}
-            />
-
-            <CaseCard
-              status="sembuh"
-              jumlah={filteredData ? filteredData["jumlah_sembuh"]["value"] : 0}
-            />
-            <CaseCard
-              status="sembuh"
-              jumlah={
-                filteredData ? filteredData["jumlah_sembuh_kum"]["value"] : 0
-              }
-              isKum={true}
-            />
-
-            <CaseCard
-              status="meninggal"
-              jumlah={
-                filteredData ? filteredData["jumlah_meninggal"]["value"] : 0
-              }
-            />
-            <CaseCard
-              status="meninggal"
-              jumlah={
-                filteredData ? filteredData["jumlah_meninggal_kum"]["value"] : 0
-              }
-              isKum={true}
-            />
-          </CaseContainer>
-        </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
